d8ff024f7a1926034d8b33d35b9e9aaa82594a58
Neil Cross <neil.cross@dragonflytechnologies.com>
2020-01-30 13:48:30 +1100

    Allow specification of root password.

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-
 # Put any tasks you would like to have carried
 # out when the container is first created here
-
-# Set the root passwd - grep docker logs for it
-ROOT_PASSWORD=`pwgen -c -n -1 12`
+# Set the root passwd from env var or gen a random one
+if [[ -z "${ROOT_PASSWORD}" ]]; then
+ ROOT_PASSWORD=`pwgen -c -n -1 12`
+fi
 echo "root:$ROOT_PASSWORD" | chpasswd
 echo "root login password: $ROOT_PASSWORD"
 
-# Also echo out the pg password written to
-# /PGPASSWORD.txt when the image was made
-# by start-postgres.sh
 
 # Launch supervisor
 supervisord -n
